e9e524a2fd8b29b756b3484e6a0ffcef34e949a9
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-09-18 20:06:44 +0200

    fixed deployment?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const phoneRoute = require("./routes/phonesRoute");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(
