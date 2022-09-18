a06b4723555f99618652ce4a9db9fa789e22dcda
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-09-18 19:49:02 +0200

    heroku deployment

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const phoneRoute = require("./routes/phonesRoute");
 const app = express();
 const PORT = 5000;
+const cors = require("cors");
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.options("*", cors());
 
 app.use(express.json());
 app.use("/api/v1", phoneRoute);
@@ -16,7 +25,7 @@ app.get("/", (req, res) => {
                 <li>Groceries</li>
             </ul>
             
-            <p>to reach any of the available lists go to (domain name/api/v1/"listName")</p>
+            <p>to reach any of the available lists go to => https://boiling-reef-36956.herokuapp.com/api/v1/"listName"</p>
 
             <p>Please Stay patient specially when you try to get to the groceries list XD</p>
         `
